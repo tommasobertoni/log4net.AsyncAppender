8e6609a56418f9a0b2a43f1abb26aff5c4602416
Tommaso Bertoni <tbertoni@cisalpinatours.it>
2019-04-10 23:55:03 +0200

    Added IEventJsonSerializer and IElasticsearchAsyncAppenderConfigurator for config driven appender setup.

---
+++ src/log4net.Elasticsearch.Async/IElasticsearchAsyncAppenderConfigurator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace log4net.Elasticsearch.Async
+{
+    public interface IElasticsearchAsyncAppenderConfigurator
+    {
+        void Configure(ElasticsearchAsyncAppender appender);
+    }
+}
---
+++ src/log4net.Elasticsearch.Async/IEventJsonSerializer.cs
@@ -0,0 +1,13 @@
+﻿using log4net.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace log4net.Elasticsearch.Async
+{
+    public interface IEventJsonSerializer
+    {
+        string SerializeToJson(LoggingEvent loggingEvent);
+    }
+}
